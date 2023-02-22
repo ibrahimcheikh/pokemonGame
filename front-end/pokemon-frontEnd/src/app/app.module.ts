85a7a160b7896fe84dce2b7ab71b0a5aeedaf072
ibrahimcheikh <ibrahimcheikh55@gmail.com>
2023-02-21 22:24:58 -0500

    missing HttpClient import

--- front-end/pokemon-frontEnd/src/app/app.module.ts
+++ front-end/pokemon-frontEnd/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -15,6 +16,7 @@ import { PlayerformComponent } from './playerform/playerform.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
