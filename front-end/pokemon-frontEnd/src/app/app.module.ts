8625872e834d34446ebc72d6c346c74efc114c83
ibrahimcheikh <ibrahimcheikh55@gmail.com>
2023-02-21 22:15:05 -0500

    some changes

--- front-end/pokemon-frontEnd/src/app/app.module.ts
+++ front-end/pokemon-frontEnd/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PlayerformComponent } from './playerform/playerform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerformComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: PlayerformComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
